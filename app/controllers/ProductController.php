9919e64821f1b50793deded7689cce24bbf419ed
jeremyraymond <jeremy.r.raymond@gmail.com>
2016-02-07 20:27:40 -0500

    Add result limits, pagination, and orderby
    Added parameters to limit the results, pagination (using an indexed
    product_id rather than OFFSET), and orderby

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -11,13 +11,21 @@ class ProductController extends Controller
 		$bot_price = ''; if(isset($_GET['bot_price'])) $bot_price = $_GET['bot_price'];
 		$in_stock = ''; if(isset($_GET['in_stock'])) $in_stock = $_GET['in_stock'];
 		$manufacturer_guid = ''; if(isset($_GET['manufacturer_guid'])) $manufacturer_guid = $_GET['manufacturer_guid'];
-		$per_page = 1000; if(isset($_GET['per_page'])) $per_page = $_GET['per_page'];
+		$last_seen = ''; if(isset($_GET['last_seen'])) $last_seen = $_GET['last_seen']; // last viewed product ID for pagination
+		$per_page = 1000; if(isset($_GET['per_page'])) $per_page = $_GET['per_page']; // number of results to return
+		$order_by = 'product_guid'; if(isset($_GET['order_by'])) $order_by = $_GET['order_by'];
+		$order = 'asc'; // asc by default, only a value of desc from $_GET['order'] will change it
+		if(isset($_GET['order'])) {
+			if($_GET['order'] == 'desc') {
+				$order = 'desc';
+			}
+		}
 
 		// build the query
 		$products = DB::table('products')
 			->join('manufacturers', 'products.manufacturer_id', '=', 'manufacturers.manufacturer_id' )
-			->select('product_guid', 'product_name', 'price', 'description', 'manufacturer_guid', 'manufacturer_name', 'amount_in_stock')
-			->where(function($products) use ($product_name, $top_price, $bot_price, $in_stock, $manufacturer_guid)
+			->select('product_id', 'product_guid', 'product_name', 'price', 'description', 'manufacturer_guid', 'manufacturer_name', 'amount_in_stock')
+			->where(function($products) use ($product_name, $top_price, $bot_price, $in_stock, $manufacturer_guid, $last_seen)
 			{
 				// conditional WHERE statements based on query string parameters
 				if ($product_name) {
@@ -35,9 +43,14 @@ class ProductController extends Controller
 				if ($manufacturer_guid) {
 					$products->where('manufacturer_guid', '=', $manufacturer_guid);
 				}
+				if ($last_seen) {
+					$products->where('product_id', '>' , $last_seen);
+				}
 			})
+			->take($per_page)
+			->orderBy($order_by, $order)
 			->get();
-		return json_encode($products);
+		return $products;
 	}
 	public function getSingleProduct($guid)
 	{
